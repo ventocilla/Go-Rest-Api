4a14e471ba0fbe199c0b3a2620dcad0d599eabd7
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-06-03 12:31:04 -0300

    storring data in the Database - INSERT

--- models/events.go
+++ models/events.go
@@ -1,9 +1,12 @@
 package models
 
-import "time"
+import (
+	"go-rest-api/db"
+	"time"
+)
 
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding: required`
 	Description string    `binding: required`
 	Location    string    `binding: required`
@@ -13,8 +16,22 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?, ?, ?, ?, ?)
+	`
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserId)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
 
 func GetAllEvents() []Event {
