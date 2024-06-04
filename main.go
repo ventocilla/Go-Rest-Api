9aedb29a184d0dd7696332fdfccf123a61925ea3
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-06-04 09:11:15 -0300

    updateing saving method in the main.go

--- main.go
+++ main.go
@@ -19,7 +19,11 @@ func main() {
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if nil != err {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events. Try again later."})
+		return
+	}
 	context.JSON(http.StatusOK, events)
 }
 
@@ -33,7 +37,12 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserId = 1
 
-	event.Save()
+	err = event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create events. Try again later."})
+		return
+	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created", "event": event})
 
