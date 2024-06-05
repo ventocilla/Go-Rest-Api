95b61362042a60cdd90b756d56fa5f0ac78eb6b6
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-06-05 12:50:54 -0300

    getting single event data by id

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-rest-api/db"
 	"go-rest-api/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +14,7 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent) // id was encoded in the path ...
 	server.POST("/events", createEvent)
 
 	server.Run(":8080") // localhost:8080
@@ -25,6 +27,20 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"m": "Could not parse event id"})
+		return
+	}
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"m": "Could not fetch event"})
+		return
+	}
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
